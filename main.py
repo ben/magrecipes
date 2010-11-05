cee1e71834c5e648f8b0295981f49786fed61b83
Ben Straub <ben@straubnet.net>
2010-11-04 20:58:02 -0700

    Removed custom login-required decorator in favor of Google's version.

--- main.py
+++ main.py
@@ -5,19 +5,13 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp.util import login_required
 
 from models import Ingredient, QuantifiedIngredient, Recipe
 
-def RequiresLoggedIn(f):
-    def callf(reqHandler, *args, **kwargs):
-        if users.get_current_user():
-            return f(reqHandler, *args, **kwargs)
-        reqHandler.redirect(users.create_login_url(reqHandler.request.uri))
-    return callf
-
 class MainHandler(webapp.RequestHandler):
 
-    #@RequiresLoggedIn
+    #@login_required
     def get(self):
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
@@ -41,34 +35,34 @@ class MainHandler(webapp.RequestHandler):
 
 
 class IngredientHandler(webapp.RequestHandler):
-    @RequiresLoggedIn
     def post(self):
-        i = Ingredient()
-        i.name = self.request.get('name')
-        i.put()
+        if users.get_current_user():
+            i = Ingredient()
+            i.name = self.request.get('name')
+            i.put()
         self.redirect('/')
 
 class RecipeHandler(webapp.RequestHandler):
-    @RequiresLoggedIn
     def post(self):
-        # Create a recipe
-        recipe = Recipe()
-        recipe.title = self.request.get('title')
-        recipe.instructions = self.request.get('instructions')
-        recipe.put()
-
-        # Create the ingredients and associate them
-        for i in xrange(1000):
-            ingredientkey = self.request.get('ingredient' + str(i))
-            if not ingredientkey: break
-            ing = Ingredient.get(ingredientkey)
-            if ing:
-                qi = QuantifiedIngredient()
-                qi.ingredient = ing
-                qi.quantity = self.request.get('amount' + str(i))
-                qi.note = self.request.get('note' + str(i))
-                qi.recipe = recipe
-                qi.put()
+        if users.get_current_user():
+            # Create a recipe
+            recipe = Recipe()
+            recipe.title = self.request.get('title')
+            recipe.instructions = self.request.get('instructions')
+            recipe.put()
+            
+            # Create the ingredients and associate them
+            for i in xrange(1000):
+                ingredientkey = self.request.get('ingredient' + str(i))
+                if not ingredientkey: break
+                ing = Ingredient.get(ingredientkey)
+                if ing:
+                    qi = QuantifiedIngredient()
+                    qi.ingredient = ing
+                    qi.quantity = self.request.get('amount' + str(i))
+                    qi.note = self.request.get('note' + str(i))
+                    qi.recipe = recipe
+                    qi.put()
         self.redirect('/')
                 
 
