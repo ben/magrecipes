49e8d93fe7297cc8489e72f427883226ffc178ec
Ben Straub <ben@straubnet.net>
2010-12-15 10:22:31 -0800

    Create-on-reference for ingredients.

--- RecipeHandler.py
+++ RecipeHandler.py
@@ -1,7 +1,7 @@
 import os
 
 from google.appengine.api import users
-from google.appengine.ext import webapp
+from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import template
 
 from models import Ingredient, QuantifiedIngredient, Recipe
@@ -11,25 +11,34 @@ from models import Ingredient, QuantifiedIngredient, Recipe
 ################################################################################
 class RecipeHandler(webapp.RequestHandler):
     def post(self):
-        if users.is_current_user_admin():
-            # Create a recipe
-            recipe = Recipe()
-            recipe.title = self.request.get('title')
-            recipe.instructions = self.request.get('instructions')
-            recipe.put()
-            
-            # Create the ingredients and associate them
-            for i in xrange(1000):
-                ingredientkey = self.request.get('ingredient' + str(i))
-                if not ingredientkey: break
-                ing = Ingredient.get(ingredientkey)
-                if ing:
-                    qi = QuantifiedIngredient()
-                    qi.ingredient = ing
-                    qi.quantity = self.request.get('amount' + str(i))
-                    qi.note = self.request.get('note' + str(i))
-                    qi.recipe = recipe
-                    qi.put()
+        if not users.is_current_user_admin():
+            self.redirect('/')
+
+         # Create a recipe
+        recipe = Recipe()
+        recipe.title = self.request.get('title')
+        recipe.instructions = self.request.get('instructions')
+        recipe.put()
+
+         # Create the ingredients and associate them
+        for i in xrange(1000):
+            # Get the ingredient's name
+            ingredient_name = self.request.get('ingredient' + str(i))
+            if not ingredient_name: break
+
+             # Fetch the ingredient, or create a new one. This runs in a transaction
+            ing_inst = Ingredient.all().filter('name =', ingredient_name).get()
+            if ing_inst is None:
+                ing_instance = Ingredient(name=ingredient_name)
+                ing_instance.put()
+
+            # Create QuantifiedIngredients for all the Ingredients in this recipe
+            qi = QuantifiedIngredient(ingredient=ing_inst,
+                                      quantity = self.request.get('amount'+str(i)),
+                                      note = self.request.get('note'+str(i)),
+                                      recipe = recipe)
+            qi.put()
+
         self.redirect('/')
                 
 
