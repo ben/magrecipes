b74a74d54e8aa7895a61cfed95d9a18810144295
Ben Straub <ben@straubnet.net>
2011-02-16 20:54:12 -0800

    Working regex search, basic results page.

--- SearchHandler.py
+++ SearchHandler.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import re
 
 from google.appengine.api import users
 from google.appengine.ext import webapp
@@ -7,19 +8,31 @@ from google.appengine.ext.webapp import template
 
 from models import Ingredient, Recipe
 
-import markdown
-
 
 ################################################################################
 class SearchHandler(webapp.RequestHandler):
     def post(self):
-        # Search recipe bodies
-
-        # Search ingredient names
+        query = self.request.get('q')
+        ingredients = []
+        recipes = []
+        if query != '':
+            # Pre-compile a regex for matching
+            regex = re.compile(query, re.I)
+            
+            # Search ingredient names
+            allingredients = Ingredient.all(keys_only=True)
+            ingredients = [i.name() for i in allingredients
+                           if regex.search(i.name())]
+            
+            # Search recipe bodies
+            allrecipes = Recipe.all()
+            recipes = [r for r in allrecipes
+                       if (regex.search(r.title) or regex.search(r.instructions))]
         
-
         template_values = {
-            'query' : self.request.get('q'),
+            'ingredients' : ingredients,
+            'recipes' : recipes,
+            'query' : query,
             }
         path = os.path.join(os.path.dirname(__file__), 'search.html')
         self.response.out.write(template.render(path, template_values))
