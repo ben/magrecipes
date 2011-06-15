9a66dd8b63c0a2462d92bba2b7323225c50d0262
Ben Straub <ben@straubnet.net>
2011-06-15 07:17:47 -0700

    Reorganization and cleanup.

--- templatefilters.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import webapp
-
-register = webapp.template.create_template_register()
-
-@register.filter
-def localcapfirst(value):
-    return "(%s)" % value #str(value).capitalize()
