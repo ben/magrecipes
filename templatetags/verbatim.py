385f1c2bf4d69c3024ca96fc0ccfd65168f65cd3
Ben Straub <ben@straubnet.net>
2011-07-11 08:52:48 -0700

    Added if_admin helper tag. Added login/logout urls and admin controls to base.html.

--- templatetags/verbatim.py
+++ templatetags/verbatim.py
@@ -11,12 +11,13 @@ blocks of jQuery templates and this will try its best
 to output the contents with no changes.
 """
 
-from django import template
+from django.template import Node, NodeList, Library
+from google.appengine.api import users
 
-register = template.Library()
+register = Library()
 
 
-class VerbatimNode(template.Node):
+class VerbatimNode(Node):
 
     def __init__(self, text):
         self.text = text
@@ -42,3 +43,26 @@ def verbatim(parser, token):
         elif token.token_type == template.TOKEN_BLOCK:
             text.append('%}')
     return VerbatimNode(''.join(text))
+
+
+
+################################################################################
+@register.tag(name="if_admin")
+def do_if_admin(parser, token):
+    nodelist_true = parser.parse(('else', 'endif',))
+    nodelist_false = NodeList()
+    token = parser.next_token()
+    if token.contents == 'else':
+        nodelist_false = parser.parse(('endif',))
+        parser.delete_first_token()
+    return IfAdminNode(nodelist_true, nodelist_false)
+
+class IfAdminNode(Node):
+    def __init__(self, nodelist_true, nodelist_false):
+        self.nodelist_true = nodelist_true
+        self.nodelist_false = nodelist_false
+
+    def render(self, context):
+        if (users.is_current_user_admin()):
+            return self.nodelist_true.render(context)
+        return self.nodelist_false.render(context)
