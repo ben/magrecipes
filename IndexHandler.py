9f6b34adb4ab520eb3ed90b35e2f8e6bc5d57428
Ben Straub <ben@straubnet.net>
2011-07-11 08:27:42 -0700

    Front page with proper hovering and month generation.

--- IndexHandler.py
+++ IndexHandler.py
@@ -8,16 +8,30 @@ from google.appengine.ext.webapp import template
 from django.utils import simplejson
 
 from models import Ingredient, Recipe, QuantifiedIngredient
-from helpers import to_dict
+from helpers import to_dict, allmonths
 
 import markdown
 
 
 
+def newMonthStruct(current, month):
+    class expando(object): pass
+    m = expando()
+    m.full = month
+    m.short = month[:3]
+    m.shortlower = month[:3].lower()
+    m.cls = ""
+    if current == month: m.cls ="current"
+    return m
+
 ################################################################################
 class IndexHandler(webapp.RequestHandler):
     def get(self):
         thismonth = date.today().strftime("%B")
+        months = [newMonthStruct(thismonth, m) for m in allmonths()]
+        months[0].cls += " tl"
+        months[-1].cls += " tr"
+        
 
         query = Recipe.gql('WHERE %s = TRUE' % thismonth.lower())
         recipes = [r for r in query.fetch(10)]
@@ -25,6 +39,7 @@ class IndexHandler(webapp.RequestHandler):
         template_values = {
             'recipes' : recipes,
             'month' : thismonth,
+            'months' : months,
             'is_admin' : users.is_current_user_admin(),
             }
         path = os.path.join(os.path.dirname(__file__), 'index.html')
