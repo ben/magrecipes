7bb2f6641ee82826f171cae9e45e4735d6b78370
Ben Straub <ben@straubnet.net>
2011-07-22 07:01:53 -0700

    Now searching tag names also.

--- SearchHandler.py
+++ SearchHandler.py
@@ -7,7 +7,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from django.template.context import RequestContext
 
-from models import Ingredient, Recipe
+from models import Ingredient, Recipe, Tag
 
 
 ################################################################################
@@ -19,9 +19,14 @@ class SearchHandler(webapp.RequestHandler):
         if query != '':
             # Pre-compile a regex for matching
             regex = re.compile(query, re.I)
+
+            # Search tag names
+            alltags = Tag.all()
+            tags = [t for t in alltags
+                    if regex.search(t.tag)]
             
             # Search ingredient names
-            allingredients = Ingredient.all(keys_only=True)
+            allingredients = Ingredient.all()
             ingredients = [i for i in allingredients
                            if regex.search(i.name())]
             
@@ -31,6 +36,7 @@ class SearchHandler(webapp.RequestHandler):
                        if (regex.search(r.title) or regex.search(r.instructions))]
         
         template_values = RequestContext(self.request, {
+            'tags' : tags,
             'ingredients' : ingredients,
             'recipes' : recipes,
             'query' : query,
