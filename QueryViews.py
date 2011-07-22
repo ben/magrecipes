bae2f6a7f0bef70008408499ba9c594eff8e3954
Ben Straub <ben@straubnet.net>
2011-07-22 06:28:15 -0700

    Tag-based query views complete.

--- QueryViews.py
+++ QueryViews.py
@@ -7,7 +7,7 @@ from google.appengine.ext.webapp import template
 from django.template.context import RequestContext
 from django.utils import simplejson
 
-from models import Recipe, Ingredient
+from models import Recipe, Ingredient, Tag
 from helpers import uniq
 
 
@@ -91,3 +91,45 @@ class RecipesByStickyHandler(webapp.RequestHandler):
                 })
         path = os.path.join(os.path.dirname(__file__), 'recipes_by_sticky.html')
         self.response.out.write(template.render(path, templatevalues))
+
+
+################################################################################
+class TagsByNameHandler(webapp.RequestHandler):
+    def get(self):
+        q = Tag.all()
+        tags = [t for t in q]
+        buckets,keys = bucketize(tags, lambda t: t.tag)
+        templatevalues = RequestContext(self.request, {
+                'tags' : tags,
+                'buckets' : buckets,
+                'keys' : keys,
+                })
+        path = os.path.join(os.path.dirname(__file__), 'tags_by_name.html')
+        self.response.out.write(template.render(path, templatevalues))
+
+
+################################################################################
+class RecipesByTagHandler(webapp.RequestHandler):
+    def get(self, tag_name):
+        t = Tag.get_by_name(tag_name)
+        if t == None:
+            # TODO: error page?
+            return
+
+        # Gather all the recipes for all the QIs for this ingredient
+        recipes = uniq([Recipe.get(key) for key in t.tagged],
+                       lambda x: x.title)
+
+        buckets,keys = bucketize(recipes, lambda x: x.title)
+        templatevalues = RequestContext(self.request, {
+                'tag' : t,
+                'recipes' : recipes,
+                'buckets' : buckets,
+                'keys' : keys
+                })
+        path = os.path.join(os.path.dirname(__file__), 'recipes_by_tag.html')
+        self.response.out.write(template.render(path, templatevalues))
+        
+
+        
+
